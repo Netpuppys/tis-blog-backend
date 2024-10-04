2cd5f82cd13c91becb1b791a08f941451a9cd712
rubayetseason <rubayetislamseason5@gmail.com>
2024-10-04 20:38:36 +0600

    schema changed

--- src/app/modules/post/post.validation.ts
+++ src/app/modules/post/post.validation.ts
@@ -5,33 +5,38 @@ const createZodPost = z.object({
     title: z.string({
       required_error: 'Title is required',
     }),
+    meta_title: z.string({
+      required_error: 'Meta title is required',
+    }),
     banner_img: z.string({
       required_error: 'Banner img is required',
     }),
     content: z.string({
       required_error: 'Content is required',
     }),
-    short_description: z.string({
-      required_error: 'Short description is required',
-    }),
-    meta_description: z.string({
-      required_error: 'Meta description is required',
-    }),
-    author_name: z.string({
-      required_error: 'Author is required',
-    }),
+    short_description: z
+      .string({
+        required_error: 'Short description is required',
+      })
+      .optional(),
+    meta_description: z
+      .string({
+        required_error: 'Meta description is required',
+      })
+      .optional(),
+    author_name: z
+      .string({
+        required_error: 'Author is required',
+      })
+      .optional(),
     slug: z.string({
       required_error: 'Slug is required',
     }),
     category_id: z.string({
       required_error: 'Category is required',
     }),
-    tags: z.array(z.string()).nonempty({
-      message: 'Tags should contain at least one tag',
-    }),
-    meta_keywords: z.array(z.string()).nonempty({
-      message: 'Meta keywords should contain at least one keyword',
-    }),
+    tags: z.array(z.string()).optional(),
+    meta_keywords: z.array(z.string()).optional(),
   }),
 });
 
@@ -40,6 +45,11 @@ const updateZodPost = z.object({
     title: z
       .string({
         required_error: 'Title is required',
+      })
+      .optional(),
+    meta_title: z
+      .string({
+        required_error: 'Meta title is required',
       })
       .optional(),
     banner_img: z
